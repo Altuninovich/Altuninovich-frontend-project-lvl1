5fdfd3daa78aa9818415f2a229186983fe3a737d
Altuninovich <altuninovich4@gmail.com>
2019-10-06 17:15:14 +0300

    1com

---
+++ dist/brain-g.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+"use strict";
+
+console.log('Welcome to the Brain Games!');
+
+var g = function g(x, y) {
+  return function (f) {
+    return f(y, x);
+  };
+};
+var f = "jora";
+var i = g(2, 3);
+console.log(i);
