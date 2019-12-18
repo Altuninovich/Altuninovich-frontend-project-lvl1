182d0bbf1215ba0b3cbaeff52e88b76820320176
altuninovich@gmail.ru <root@DESKTOP-EFJ8J1R.localdomain>
2019-12-18 19:38:12 +0300

    modiSRC

--- src/start.js
+++ src/start.js
@@ -7,7 +7,7 @@ const start = (instruction, fun) => {
   console.log(instruction);
   const yourName = readlineS.question(' May I have your name? ');
   console.log(`Hello, ${yourName}!`);
-  for (let i = 4; i > 0; i -= 1) {
+  for (let i = 3; i > 0; i -= 1) {
     const questionAnswer = fun();
     console.log(`${'Question: '}${car(questionAnswer)}`);
     const yourAnswer = readlineS.question('Your answer: ');
